1ef26a40ab1e9f7a9448aba75c352144a3325c18
Cody Brubaker and Eric Bomblatus <eric@>
2016-10-17 16:49:54 -0700

    removed alerts and added a paragragh

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,8 @@ var multiply = function(result1) {return result1 * 703;
 };
 
 var weight = parseInt(prompt("How much do you weigh in pounds?"));
-var height = parseInt(prompt("How tall are you in  square inches?"));
+var height = parseInt(prompt("How tall are you in inches?"));
 var result1 = divide(weight, height);
 var result2 = multiply(result1);
 var sentence = "Your BMI is " + result2 + ".";
-alert(result1);
-alert(result2);
 alert(sentence);
