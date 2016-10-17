a8e14bc4829f4a2559630f80f252cb9096f32b6e
Cody Brubaker and Eric Bomblatus <eric@>
2016-10-17 13:28:20 -0700

    add initial file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var divide = function(weight, height) { return weight / Math.pow(height, 2);
+};
+
+var multiply = function(result1) {return result1 * 703;
+};
+
+var weight = parseInt(prompt("How much do you weigh in pounds?"));
+var height = parseInt(prompt("How tall are you in  square inches?"));
+var result1 = divide(weight, height);
+var result2 = multiply(result1);
+var sentence = "Your BMI is " + result2 + ".";
+alert(result1);
+alert(result2);
+alert(sentence);
